e70f899aaf63bb4fc515f74ff1dda6c3b20faa64
Ibrahima <44869843+Ibra-Yves@users.noreply.github.com>
2020-03-12 08:52:30 +0100

    Add files via upload

--- inc/accueil.php
+++ inc/accueil.php
@@ -4,25 +4,19 @@
     <title>Centre d'Aéromodélisme de Pepinster ASBL</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <link href="./images/accueil/logo.jpg" rel="icon" type="image/x-icon" />
     
     <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
-
     <link rel="stylesheet" href="./css/open-iconic-bootstrap.min.css">
     <link rel="stylesheet" href="./css/animate.css">
     
     <link rel="stylesheet" href="./css/owl.carousel.min.css">
     <link rel="stylesheet" href="./css/owl.theme.default.min.css">
     <link rel="stylesheet" href="./css/magnific-popup.css">
-
     <link rel="stylesheet" href="./css/aos.css">
-
     <link rel="stylesheet" href="./css/ionicons.min.css">
-
     <link rel="stylesheet" href="./css/bootstrap-datepicker.css">
     <link rel="stylesheet" href="./css/jquery.timepicker.css">
-
     
     <link rel="stylesheet" href="./css/flaticon.css">
     <link rel="stylesheet" href="./css/icomoon.css">
@@ -33,6 +27,9 @@
 	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
         <a class="navbar-brand" href="./index">CAPON <span>WEB</span></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
 	          <li class="nav-item active"><a href="./index" class="nav-link">Accueil</a></li>
@@ -111,7 +108,6 @@
         </div>
       </div>
     </div>
-
     <section class="ftco-section">
       <div class="container">
       	<div class="row justify-content-center">
@@ -167,7 +163,6 @@
         </div>
       </div>
     </section>
-
     <section class="ftco-section services-section img" style="background-image: url(./images/accueil/site_CAP.jpg);">
     	<div class="overlay"></div>
     	<div class="container">
@@ -216,7 +211,6 @@
     		</div>
     	</div>
     </section>
-
     <section class="ftco-section">
       <div class="container">
         <div class="row justify-content-center mb-5">
@@ -267,7 +261,6 @@
         </div>
       </div>
     </section>		
-
     <footer class="ftco-footer ftco-bg-dark ftco-section">
       <div class="container">
         <div class="row mb-5">
@@ -304,7 +297,6 @@
           
       </div>
     </footer>
-
   <script src="./js/jquery.min.js"></script>
   <script src="./js/jquery-migrate-3.0.1.min.js"></script>
   <script src="./js/popper.min.js"></script>
