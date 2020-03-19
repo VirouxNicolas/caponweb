b3ed50659b29cd50d28444c14252151954da3031
VirouxNicolas <33083942+VirouxNicolas@users.noreply.github.com>
2020-03-19 14:08:51 +0100

    Delete db.php

--- db/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost","root","","caponweb");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Impossible de se connecter à la base de données: " . mysqli_connect_error();
-  }
-?>
