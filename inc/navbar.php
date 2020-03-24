89c99a0c60ab293dfbfcaa651a7962ef8f904f2c
VirouxNicolas <33083942+VirouxNicolas@users.noreply.github.com>
2020-03-24 14:53:30 +0100

    Final

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
-        <a class="navbar-brand" href="./index">CAPON <span>WEB</span></a>
+        <a class="navbar-brand" href="../index.php">CAPON <span>WEB</span></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	        <span class="oi oi-menu"></span> Menu
 	      </button>
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item active"><a href="./accueil.php" class="nav-link">Accueil</a></li>
+	          <li class="nav-item active"><a href="../index.php" class="nav-link">Accueil</a></li>
 	          <li class="nav-item"><a href="club" class="nav-link">Le Club</a></li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
