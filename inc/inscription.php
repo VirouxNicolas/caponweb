6ea1d8be783910a7154d9540c296427c59ba69f7
Ibrahima <44869843+Ibra-Yves@users.noreply.github.com>
2020-04-11 11:08:58 +0200

    Update inscription.php
    affichage message d'erreur

--- inc/inscription.php
+++ inc/inscription.php
@@ -1,4 +1,23 @@
-   
+<?php
+      require('./db/db.php');
+      if (isset($_REQUEST['username'])){
+          $username = stripslashes($_REQUEST['username']); // enlever backslashes
+          $username = mysqli_real_escape_string($con,$username); //escapes special characters in a string
+          $email = stripslashes($_REQUEST['email']);
+          $email = mysqli_real_escape_string($con,$email);
+          $password = stripslashes($_REQUEST['password']);
+          $password = mysqli_real_escape_string($con,$password);
+                    
+          $trn_date = date("Y-m-d H:i:s");
+          $query = "INSERT into `users` (username, password, email, trn_date) VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
+          $result = mysqli_query($con,$query);
+            if($result){
+                $messageOK = "<div class='form'><h3>Vous êtes enregistré avec succès.</h3></div>";
+            }else{
+                $messageErreur = "maxime t'es une pute";
+            }
+      }
+?>
     <div class="site-section" id="connexion-section">
           <div class="container">
             <div class="row">
@@ -10,6 +29,8 @@
             </div>
             <div class="row">
               <div class="col-lg-12 mb-5" data-aos="fade-up" data-aos-delay="100">
+              <?php if(isset($messageOK)){ ?><div class="alert alert-success" role="alert"> <?php echo $messageOK; ?> </div><?php } ?>
+              <?php if(isset($messageErreur)){ ?><div class="alert alert-danger" role="alert"> <?php echo $messageErreur; ?> </div><?php } ?>
                 <form action="" method="post" id="login-form" name="login">
                     <div class="form-group row">
                         <div class="col-md-6 mb-4 mb-lg-0">
@@ -32,26 +53,6 @@
                     </div>
                   </div>
                 </form>
-                <?php
-                    require('./db/db.php');
-                    if (isset($_REQUEST['username'])){
-                        $username = stripslashes($_REQUEST['username']); // enlever backslashes
-                        $username = mysqli_real_escape_string($con,$username); //escapes special characters in a string
-                        $email = stripslashes($_REQUEST['email']);
-                        $email = mysqli_real_escape_string($con,$email);
-                        $password = stripslashes($_REQUEST['password']);
-                        $password = mysqli_real_escape_string($con,$password);
-                    
-                        $trn_date = date("Y-m-d H:i:s");
-                            $query = "INSERT into `users` (username, password, email, trn_date) VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
-                            $result = mysqli_query($con,$query);
-                            if($result){
-                                echo "<div class='form'><h3>Vous êtes enregistré avec succès.</h3><br/>Cliquez ici pour vous connecter <a href='connexion.php'>Se connecter</a></div>";
-                            }else{
-                                echo "maxime t'es une pute";
-                            }
-                        }
-                ?>
               </div>
             </div>
           </div>
