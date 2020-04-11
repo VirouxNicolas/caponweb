92d84edf47408ca499ccf52d9d91f25370aca395
VirouxNicolas <33083942+VirouxNicolas@users.noreply.github.com>
2020-04-11 12:31:24 +0200

    Update prix.php

--- inc/prix.php
+++ inc/prix.php
@@ -1,75 +1,35 @@
-<div class="site-section bg-light" id="team">
+<div class="site-section bg-light" id="prix">
         <div class="container">
           <div class="row mb-5 justify-content-center text-center">
             <div class="col-md-7">
               <div class="block-heading-1" data-aos="fade-up" data-aos-delay="">
-                <h2>Pricing</h2>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+                <h2>Devenir membre</h2>
+                <p>Vous voulez vous joindre à nous afin de pratiquer le vol circulaire? <br>Ne tardez pas, voici la prix de notre cotisation et ce qu'elle inclus!</p>
               </div>
             </div>
           </div>
-          <div class="row mb-5">
-            <div class="col-md-6 mb-4 mb-lg-0 col-lg-4" data-aos="fade-up" data-aos-delay="">
+          
+ 
+            <div class="row mb-5 justify-content-center text-center" data-aos="fade-up" data-aos-delay="100">
               <div class="pricing">
-                <h3 class="text-center text-black">Basic</h3>
+                <h3 class="text-center text-black">Membre annuel</h3>
                 <div class="price text-center mb-4 ">
-                  <span><span>$47</span> / year</span>
+                  <span><span>€115</span> / an</span>
                 </div>
                 <ul class="list-unstyled ul-check success mb-5">
-
-                  <li>Officia quaerat eaque neque</li>
-                  <li>Possimus aut consequuntur incidunt</li>
-                  <li class="remove">Lorem ipsum dolor sit amet</li>
-                  <li class="remove">Consectetur adipisicing elit</li>
-                  <li class="remove">Dolorum esse odio quas architecto sint</li>
+ 
+                  <li>Accès aux infrastructures du club</li>
+                  <li>Inscription à la fédération</li>
                 </ul>
-                <p class="text-center">
-                  <a href="#" class="btn btn-secondary btn-md">Buy Now</a>
+                
+                  <p class="text-center">
+                  <a href="#" class="btn btn-primary btn-md text-white">Inscrivez-vous</a>
                 </p>
+                
               </div>
             </div>
 
-            <div class="col-md-6 mb-4 mb-lg-0 col-lg-4" data-aos="fade-up" data-aos-delay="100">
-              <div class="pricing">
-                <h3 class="text-center text-black">Premium</h3>
-                <div class="price text-center mb-4 ">
-                  <span><span>$200</span> / year</span>
-                </div>
-                <ul class="list-unstyled ul-check success mb-5">
 
-                  <li>Officia quaerat eaque neque</li>
-                  <li>Possimus aut consequuntur incidunt</li>
-                  <li>Lorem ipsum dolor sit amet</li>
-                  <li>Consectetur adipisicing elit</li>
-                  <li class="remove">Dolorum esse odio quas architecto sint</li>
-                </ul>
-                <p class="text-center">
-                  <a href="#" class="btn btn-primary btn-md text-white">Buy Now</a>
-                </p>
-              </div>
-            </div>
-
-            <div class="col-md-6 mb-4 mb-lg-0 col-lg-4" data-aos="fade-up" data-aos-delay="200">
-              <div class="pricing">
-                <h3 class="text-center text-black">Professional</h3>
-                <div class="price text-center mb-4 ">
-                  <span><span>$750</span> / year</span>
-                </div>
-                <ul class="list-unstyled ul-check success mb-5">
-
-                  <li>Officia quaerat eaque neque</li>
-                  <li>Possimus aut consequuntur incidunt</li>
-                  <li>Lorem ipsum dolor sit amet</li>
-                  <li>Consectetur adipisicing elit</li>
-                  <li>Dolorum esse odio quas architecto sint</li>
-                </ul>
-                <p class="text-center">
-                  <a href="#" class="btn btn-secondary btn-md">Buy Now</a>
-                </p>
-              </div>
-            </div>
-          </div>
-
-
+ 
         </div>
       </div>
