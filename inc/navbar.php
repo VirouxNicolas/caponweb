c536c9b3147f0995002d901f6b0b42bcc15ea308
VirouxNicolas <33083942+VirouxNicolas@users.noreply.github.com>
2020-04-11 12:32:41 +0200

    navbar

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,22 +1,21 @@
 <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
-
+ 
         <div class="container">
           <div class="row align-items-center position-relative">
-
-
+ 
             <div class="site-logo">
               <a href="index.php#home-section" class="text-black"><span class="text-primary">CaponWeb</a>
             </div>
-
+ 
             <div class="col-12">
               <nav class="site-navigation text-right ml-auto " role="navigation">
-
+ 
                 <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
                   <?php 
                   if(empty($_GET)){
                     echo '
                     <li><a href="index.php#home-section" class="nav-link">Accueil</a></li>
-
+ 
                     
                     <li class="has-children">
                       <a href="#services-section" class="nav-link">Notre club</a>
@@ -29,14 +28,15 @@
                     <a href="#about-section" class="nav-link">Le vol circulaire</a>
                     <ul class="dropdown arrow-top">
                       <li><a href="#acrobatie" class="nav-link">Acrobatie</a></li>
-                      <li><a href="#team" class="nav-link">La course par équipe</a></li> 
+                      <li><a href="#vitesse" class="nav-link">La course par équipe</a></li> 
                       <li><a href="#vitesse" class="nav-link">La vitesse</a></li>
                     </ul>
                   </li>
                     <li><a href="index.php?name=internationalContest" class="nav-link">International Contest</a></li>
                     
                     <li><a href="index.php?name=contact" class="nav-link">Contact</a></li>
-
+                    <li><a href="#prix" class="nav-link">Devenir membre</a></li> 
+ 
                     <li class="has-children">
                       <a class="nav-link">Connexion</a>
                       <ul class="dropdown arrow-top">
@@ -55,12 +55,12 @@
                   ?>
                   </ul>
               </nav>
-
-            </div>
-
+                    
+                        </div>
+ 
             <div class="toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
-
+ 
           </div>
         </div>
-
+ 
       </header>
