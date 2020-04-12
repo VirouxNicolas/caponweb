f4702ceea1cd10e26c56e033cde2389fafbe0067
Liber Maxime <44001018+LiberTMx@users.noreply.github.com>
2020-04-12 13:14:24 +0200

    Update prix.php

--- inc/prix.php
+++ inc/prix.php
@@ -9,17 +9,114 @@
             </div>
           </div>
           
- 
-            <div class="row mb-5 justify-content-center text-center" data-aos="fade-up" data-aos-delay="100">
+          <div class="row mb-5 justify-content-center text-center" data-aos="fade-up" data-aos-delay="100">
               <div class="pricing">
-                <h3 class="text-center text-black">Membre annuel</h3>
+                <h3 class="text-center text-black">Membre adhérent (- 18 ans) </h3>
                 <div class="price text-center mb-4 ">
-                  <span><span>€115</span> / an</span>
+                  <span><span>€20</span> / an</span>
                 </div>
                 <ul class="list-unstyled ul-check success mb-5">
  
                   <li>Accès aux infrastructures du club</li>
-                  <li>Inscription à la fédération</li>
+                </ul>
+                
+                  <p class="text-center">
+                  <a href="#" class="btn btn-primary btn-md text-white">Inscrivez-vous</a>
+                </p>
+                
+              </div>
+              <div class="pricing">
+                <h3 class="text-center text-black">Membre adhérent (+ 18 ans) </h3>
+                <div class="price text-center mb-4 ">
+                  <span><span>€45</span> / an</span>
+                </div>
+                <ul class="list-unstyled ul-check success mb-5">
+ 
+                  <li>Accès aux infrastructures du club</li>
+                </ul>
+                
+                  <p class="text-center">
+                  <a href="#" class="btn btn-primary btn-md text-white">Inscrivez-vous</a>
+                </p>
+                
+              </div>
+            </div>
+            <div class="row mb-5 justify-content-center text-center" data-aos="fade-up" data-aos-delay="100">
+              <div class="pricing">
+                <h3 class="text-center text-black">Membre pratiquant (-18 ans)</h3>
+                <div class="price text-center mb-4 ">
+                  <span><span>€30</span> / an</span>
+                </div>
+                <ul class="list-unstyled ul-check success mb-5">
+ 
+                  <li>Accès aux infrastructures du club</li>
+                  <li>Inclus l'inscription à la fédération (12€)</li>
+                </ul>
+                
+                  <p class="text-center">
+                  <a href="#" class="btn btn-primary btn-md text-white">Inscrivez-vous</a>
+                </p>
+                
+              </div>
+              <div class="pricing">
+                <h3 class="text-center text-black">Membre pratiquant (+18 ans)</h3>
+                <div class="price text-center mb-4 ">
+                  <span><span>€70</span> / an</span>
+                </div>
+                <ul class="list-unstyled ul-check success mb-5">
+ 
+                  <li>Accès aux infrastructures du club</li>
+                  <li>Inclus l'inscription à la fédération (25€)</li>
+                </ul>
+                
+                  <p class="text-center">
+                  <a href="#" class="btn btn-primary btn-md text-white">Inscrivez-vous</a>
+                </p>
+                
+              </div>
+            </div>
+            <div class="row mb-5 justify-content-center text-center" data-aos="fade-up" data-aos-delay="100">
+              <div class="pricing">
+                <h3 class="text-center text-black">Cotisation familiale</h3>
+                <div class="price text-center mb-4 ">
+                  <span><span>€70</span> / an</span>
+                </div>
+                <ul class="list-unstyled ul-check success mb-5">
+ 
+                  <li>Accès aux infrastructures du club</li>
+                  <li>Inclus l'inscription à la fédération (12€)</li>
+                </ul>
+                
+                  <p class="text-center">
+                  <a href="#" class="btn btn-primary btn-md text-white">Inscrivez-vous</a>
+                </p>
+                
+              </div>
+              
+            </div>
+            <div class="row mb-5 justify-content-center text-center" data-aos="fade-up" data-aos-delay="100">
+              <div class="pricing">
+                <h3 class="text-center text-black">Membre d'honneur</h3>
+                <div class="price text-center mb-4 ">
+                  <span><span>€25</span> / an</span>
+                </div>
+                <ul class="list-unstyled ul-check success mb-5">
+                  <li>Accès aux infrastructures du club</li>
+                </ul>
+                
+                  <p class="text-center">
+                  <a href="#" class="btn btn-primary btn-md text-white">Inscrivez-vous</a>
+                </p>
+                
+              </div>
+              <div class="pricing">
+                <h3 class="text-center text-black">Membre juge fédération</h3>
+                <div class="price text-center mb-4 ">
+                  <span><span>€35</span> / an</span>
+                </div>
+                <ul class="list-unstyled ul-check success mb-5">
+ 
+                  <li>Accès aux infrastructures du club</li>
                 </ul>
                 
                   <p class="text-center">
