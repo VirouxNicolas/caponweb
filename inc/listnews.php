b5337a8eb11f32ff5c43bf4ef9308f0e7d8b7c41
Ibrahima <44869843+Ibra-Yves@users.noreply.github.com>
2020-04-20 18:07:13 +0200

    Update listnews.php

--- inc/listnews.php
+++ inc/listnews.php
@@ -1,10 +1,18 @@
-<a href="index.php?name=news" class="nav-link">Creer une News</a>
-
-<body>
-    <h1>Derniers articles</h1>
-    <div id="idNews">
-    </div>
-
-    <script src="js/ajax.js"></script>
-    <script src="js/validarticle.js"></script>
-</body>
+<section class="ftco-section">
+    <div class="container">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10 text-center d-flex ftco-animate">
+        <div class="blog-entry justify-content-end">
+            <img src="./images/accueilImage.jpg" alt="Image" class="img-thumbnail">
+            <div class="text pt-4">
+            <div class="meta mb-3">
+                <span id="idNews" class="d-block text-muted"></span>
+            </div>
+            <h2 id="idNews" class="heading mt-2"></h2>
+            <p id="idNews"></p>
+            </div>
+        </div>
+        </div>
+</section>
+<script src="js/ajax.js"></script>
+<script src="js/validarticle.js"></script>
