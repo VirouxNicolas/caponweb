a1db601b4f5aec05d1de80f537dbb8c074764c9e
Ibrahima <44869843+Ibra-Yves@users.noreply.github.com>
2020-04-20 18:08:09 +0200

    Update validarticle.js

--- js/validarticle.js
+++ js/validarticle.js
@@ -1,14 +1,17 @@
 var articlesElt = document.getElementById("idNews");
-ajaxGet("http://51.75.126.61/db/news.php", function (reponse) {
+ajaxGet("http://51.75.126.61/db/news.php/1", function (reponse) {
     // Transforme la réponse en un tableau d'articles
     var articles = JSON.parse(reponse);
     articles.forEach(function (article) {
-        // Ajout du titre et du contenu de chaque article
         
+        // Ajout du titre et du contenu de chaque article
+        var nameElt = document.createElement("span");
+        nameElt.textContent = article.name;
         var titreElt = document.createElement("h2");
         titreElt.textContent = article.subject;
         var contenuElt = document.createElement("p");
         contenuElt.textContent = article.descriptionArticles;
+        articlesElt.appendChild(nameElt);
         articlesElt.appendChild(titreElt);
         articlesElt.appendChild(contenuElt);
     });
