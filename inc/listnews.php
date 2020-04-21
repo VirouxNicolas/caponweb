482bb19290af83d74d9ee4e8653acddc84866dd0
Ibrahima <ibrahima.conde@hotmail.com>
2020-04-21 16:49:26 +0200

    affichage news
    affichage news

--- inc/listnews.php
+++ inc/listnews.php
@@ -3,13 +3,14 @@
     <div class="row d-flex justify-content-center">
         <div class="col-md-10 text-center d-flex ftco-animate">
         <div class="blog-entry justify-content-end">
-            <img src="./images/accueilImage.jpg" alt="Image" class="img-thumbnail">
+            <div id="articles">
             <div class="text pt-4">
             <div class="meta mb-3">
                 <span id="idNews" class="d-block text-muted"></span>
             </div>
             <h2 id="idNews" class="heading mt-2"></h2>
             <p id="idNews"></p>
+            <h5 id="idNews"></h5>
             </div>
         </div>
         </div>
--- js/validarticle.js
+++ js/validarticle.js
@@ -5,14 +5,20 @@ ajaxGet("http://localhost/caponweb/db/news.php/1", function (reponse) {
     articles.forEach(function (article) {
         
         // Ajout du titre et du contenu de chaque article
+        var logoElt = document.createElement("img");
+        logoElt.src = article.pictures;
         var nameElt = document.createElement("span");
         nameElt.textContent = article.name;
         var titreElt = document.createElement("h2");
         titreElt.textContent = article.subject;
         var contenuElt = document.createElement("p");
         contenuElt.textContent = article.descriptionArticles;
+        var dateElt = document.createElement("h5");
+        dateElt.textContent = article.date_creation;
+        articlesElt.appendChild(logoElt);
         articlesElt.appendChild(nameElt);
         articlesElt.appendChild(titreElt);
         articlesElt.appendChild(contenuElt);
+        articlesElt.appendChild(dateElt);
     });
 });
