3aeb7c6d0114f76c87153b1f33c9b1af2b8b45d4
VirouxNicolas <33083942+VirouxNicolas@users.noreply.github.com>
2020-05-10 16:15:48 +0200

    up srv

--- js/allarticle.js
+++ js/allarticle.js
@@ -1,5 +1,5 @@
 var articlesElt = document.getElementById("idNews");
-ajaxGet("http://localhost/Caponweb1/db/news.php", function (reponse) {
+ajaxGet("http://51.75.126.61/db/news.php", function (reponse) {
     // Transforme la réponse en un tableau d'articles
     var articles = JSON.parse(reponse);
     articles.forEach(function (article) {
