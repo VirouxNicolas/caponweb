9ad493f641d389a7e442a55a1ce6c6463127bb2d
VirouxNicolas <33083942+VirouxNicolas@users.noreply.github.com>
2020-05-10 16:06:11 +0200

    srv

---
+++ api/db.php
@@ -0,0 +1,11 @@
+<?php
+	$server = "51.75.126.61";
+	$username = "admin";
+	$password = "";
+	$db = "caponweb";
+	$con = mysqli_connect($server, $username, $password, $db);
+	if (mysqli_connect_errno())
+  {
+  echo "Impossible de se connecter à la base de données: " . mysqli_connect_error();
+  }
+?>
