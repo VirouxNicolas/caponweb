f92bfc9c6b7d3fdf9a59b514647bd8b52aebbf83
VirouxNicolas <33083942+VirouxNicolas@users.noreply.github.com>
2020-05-10 19:05:45 +0200

    up

--- api/api.php
+++ api/api.php
@@ -17,14 +17,14 @@ class API{
      * connection à la db
      */
     function dbConnection(){
-        $this->connect = new PDO("mysql:host=localhost;dbname=caponweb", "root", "");
+        $this->connect = new PDO("mysql:host=51.75.126.61;dbname=caponweb", "youraccount", "password");
     }
 
     /**
      * @return mixed
      */
     function outputAllNews(){
-        $select = $this->connect->prepare("SELECT * FROM newsArticles ORDER BY idNews");
+        $select = $this->connect->prepare("SELECT * FROM newsarticles ORDER BY idNews");
         if($select->execute()){
             while($row = $select->fetch(PDO::FETCH_ASSOC)){
                 $data[] = $row;
@@ -40,7 +40,7 @@ class API{
      * @return mixed
      */
     function outputValidNews(){
-        $select = $this->connect->prepare("SELECT * FROM newsArticles WHERE confirmation = 1");
+        $select = $this->connect->prepare("SELECT * FROM newsarticles WHERE confirmation = 1");
         if($select->execute()){
             while($row = $select->fetch(PDO::FETCH_ASSOC)){
                 $data[] = $row;
@@ -55,7 +55,7 @@ class API{
      * @return mixed
      */
     function outputWaitingNews(){
-        $select = $this->connect->prepare("SELECT * FROM newsArticles WHERE confirmation = 0");
+        $select = $this->connect->prepare("SELECT * FROM newsarticles WHERE confirmation = 0");
         if($select->execute()){
             while($row = $select->fetch(PDO::FETCH_ASSOC)){
                 $data[] = $row;
