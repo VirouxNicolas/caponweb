5def07255707917fa062435eb2ffc5fe40efccb4
VirouxNicolas <33083942+VirouxNicolas@users.noreply.github.com>
2020-05-19 19:24:27 +0200

    Create dash_articles.php

---
+++ inc/dash_articles.php
@@ -0,0 +1,89 @@
+<section id="main-content">
+      <section class="wrapper">
+        <h3><i class="fa fa-angle-right"></i>ADMINISTRATION ARTICLES</h3>
+        <div class="row mb">
+          <!-- page start-->
+          <div class="content-panel">
+            <div class="adv-table">
+              <table cellpadding="0" cellspacing="0" border="0" class="display table table-bordered" id="hidden-table-info">
+                <thead>
+                  <tr>
+                    <th>id</th>
+                    <th>Auteur</th>
+                    <th>Mail auteur</th>
+                    <th>Sujet</th>
+                    <th>Description</th>
+                    <th>Etat</th>
+                    <th>Cacher</th>
+                    <th>Publier</th>
+                    <!--<th>Date</th>-->
+                    <th>Supprimer</th>
+                    
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="gradeX">
+                    <?php
+                          $client = curl_init('http://51.75.126.61/api/apiHandler.php?action=outputAllNews');
+                          
+                          curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
+                          $response = curl_exec($client);
+                          $result = json_decode($response);
+
+                          $output = '';
+
+                          if(count($result) > 0){
+                              foreach($result as $row){
+                                echo "<td>$row->idNews</td>";
+                                  echo "<td>$row->name</td>";
+                                  echo "<td>$row->mail</td>";
+                                  echo "<td>$row->subject</td>";
+                                  echo "<td>$row->descriptionArticles</td>";
+                                  echo "<td>$row->confirmation</td>";
+                                  echo "<td><a href='http://51.75.126.61/api/apiHandler.php?action=updateCacheArticle&idNews=$row->idNews'<button class='btn'><i class='fa fa-eye-slash'></i></button></a></td>";
+                                  echo "<td><a href='http://51.75.126.61/api/apiHandler.php?action=updateVisibleArticle&idNews=$row->idNews'<button class='btn'><i class='fa fa-eye'></i></button></a></td>";
+                                  echo "<td><a href='http://51.75.126.61/api/apiHandler.php?action=deleteArticle&idNews=$row->idNews'<button class='btn'><i class='fa fa-trash'></i></button></a></td>";
+                                  echo "</tr>"; 
+              
+                              }
+                          }else{
+                              $output .= '<tr><td colspan="3" align="center">Not found!</td></tr>';
+                          }
+
+                          echo $output;
+                      ?>
+                  
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <!-- page end-->
+        </div>
+        <!-- /row -->
+      </section>
+      <!-- /wrapper -->
+    </section>
+
+    <!-- <script>
+        $(document).ready(function(){
+
+            $('#contactForm').on('submit', function(event){
+                event.preventDefault();
+                if($('#name').val() == ''){
+                    alert('Nom field is required!');
+                }else{
+                    var formData = $(this).serialize();
+                    $.ajax({
+                        url: "controller/update_confirmation_article.php",
+                        method: "POST",
+                        data: formData,
+                        success:function(data){
+                            //outputData();
+                            $('#contactForm')[0].reset();
+                        }
+                    });
+                }
+            });
+
+        });
+    </script> -->
