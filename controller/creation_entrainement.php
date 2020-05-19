062eefbeb4e420ca581d2a15a668b8ec214abc17
VirouxNicolas <33083942+VirouxNicolas@users.noreply.github.com>
2020-05-19 19:21:25 +0200

    Create creation_entrainement.php

---
+++ controller/creation_entrainement.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_POST["action"])){
+    if($_POST["action"] == 'addEntrainement'){
+        $data = array(
+            'lieu' => $_POST["lieu"],
+            'date_entrainement' => $_POST["date_entrainement"],
+            'heure_debut'      => $_POST["heure_debut"],
+            'heure_fin'      => $_POST["heure_fin"]
+        );
+
+       $client = curl_init('http://51.75.126.61/api/apiHandler.php?action=addEntrainement');
+        curl_setopt($client, CURLOPT_POST, true);
+        curl_setopt($client, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($client);
+        curl_close($client);
+ 
+    }
+}
+
+?>
