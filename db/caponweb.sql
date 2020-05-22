533284fa92dfa7b5411de3f5b8d3049058f9442f
VirouxNicolas <33083942+VirouxNicolas@users.noreply.github.com>
2020-05-22 17:08:18 +0200

    Update caponweb.sql

--- db/caponweb.sql
+++ db/caponweb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 20, 2020 at 04:43 PM
+-- Generation Time: May 19, 2020 at 02:06 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.18
 
@@ -22,26 +22,6 @@ SET time_zone = "+00:00";
 -- Database: `caponweb`
 --
 
-DELIMITER $$
---
--- Procedures
---
-DROP PROCEDURE IF EXISTS `GetUserByEmail`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `GetUserByEmail` (IN `mail` VARCHAR(255))  BEGIN
-	SELECT * FROM users WHERE email = mail;
-END$$
-
-DROP PROCEDURE IF EXISTS `GetUsers`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `GetUsers` (IN `users` VARCHAR(255))  BEGIN
-	SELECT * FROM users;
-END$$
-
-DROP PROCEDURE IF EXISTS `insertNews`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `insertNews` (IN `name` VARCHAR(255), IN `mail` VARCHAR(255), IN `subject` VARCHAR(255), IN `description` VARCHAR(255), IN `pictures` VARCHAR(255), IN `confirmation` BOOLEAN)  NO SQL
-INSERT into newsarticles (name,mail,subject,descriptionArticles,pictures, confirmation) VALUES (name, mail, subject, description, pictures, confirmation)$$
-
-DELIMITER ;
-
 -- --------------------------------------------------------
 
 --
@@ -50,22 +30,36 @@ DELIMITER ;
 
 DROP TABLE IF EXISTS `entrainement`;
 CREATE TABLE IF NOT EXISTS `entrainement` (
-  `entrainementID` int(11) NOT NULL AUTO_INCREMENT,
-  `id` int(11) NOT NULL,
-  `lieu` varchar(50) DEFAULT NULL,
-  `date` date DEFAULT NULL,
-  `heureDebut` time DEFAULT NULL,
-  `heureFin` time DEFAULT NULL,
-  PRIMARY KEY (`entrainementID`),
-  KEY `id` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `idEntrainement` int(11) NOT NULL AUTO_INCREMENT,
+  `lieu` varchar(50) NOT NULL,
+  `date_entrainement` date DEFAULT NULL,
+  `heure_debut` time DEFAULT NULL,
+  `heure_fin` time DEFAULT NULL,
+  `idUsers` int(11) NOT NULL,
+  PRIMARY KEY (`idEntrainement`),
+  KEY `idUsers` (`idUsers`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `entrainement`
+-- Table structure for table `inscription_contest`
 --
 
-INSERT INTO `entrainement` (`entrainementID`, `id`, `lieu`, `date`, `heureDebut`, `heureFin`) VALUES
-(1, 1, 'Avenue du ciseau', '2020-03-18', '13:12:00', '15:13:00');
+DROP TABLE IF EXISTS `inscription_contest`;
+CREATE TABLE IF NOT EXISTS `inscription_contest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `prenom` varchar(50) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `choixSJ` varchar(50) DEFAULT NULL,
+  `licence` varchar(100) DEFAULT NULL,
+  `category` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `reservation` int(11) DEFAULT NULL,
+  `message` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -76,24 +70,24 @@ INSERT INTO `entrainement` (`entrainementID`, `id`, `lieu`, `date`, `heureDebut`
 DROP TABLE IF EXISTS `newsarticles`;
 CREATE TABLE IF NOT EXISTS `newsarticles` (
   `idNews` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `mail` varchar(100) NOT NULL,
-  `subject` varchar(50) NOT NULL,
-  `descriptionArticles` varchar(255) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `mail` varchar(50) DEFAULT NULL,
+  `subject` varchar(50) DEFAULT NULL,
+  `descriptionArticles` varchar(255) DEFAULT NULL,
   `pictures` varchar(255) DEFAULT NULL,
-  `confirmation` tinyint(1) NOT NULL,
+  `confirmation` tinyint(4) DEFAULT NULL,
+  `date_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`idNews`)
-) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `newsarticles`
 --
 
-INSERT INTO `newsarticles` (`idNews`, `name`, `mail`, `subject`, `descriptionArticles`, `pictures`, `confirmation`) VALUES
-(11, 'maxime', 'mxm.lbr@outlook.com', 'La vitesse infinie des avions', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ', './images/cargo_air_big.jpg', 1),
-(17, 'Nico', 'mxm.lbr@outlook.com', 'Du jamais vu', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ', '', 1),
-(16, 'Max', 'mxm.lbr@outlook.com', 'vol spectaculaire', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ', '', 1),
-(15, 'Ibrahima', 'ib.conde@hotmail.com', 'Course a pied', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ', '', 0);
+INSERT INTO `newsarticles` (`idNews`, `name`, `mail`, `subject`, `descriptionArticles`, `pictures`, `confirmation`, `date_creation`) VALUES
+(1, 'Ibra', 'ibra@hotmail.com', 'qui suis je', 'depuis mon jeune, j\'aime rigoler, je deteste Maxime car c\'est un idiot', NULL, 1, '2020-05-19 13:57:30'),
+(2, 'Ibra', 'ibra@hotmail.com', 'qui suis je', 'depuis mon jeune, j\'aime rigoler, je deteste Maxime car c\'est un idiot', NULL, 1, '2020-05-19 13:57:33'),
+(3, 'test', 'ibrahima.conde@hotmail.com', 'test', 'est-ce que cela marche', NULL, 1, '2020-05-19 13:58:23');
 
 -- --------------------------------------------------------
 
@@ -104,31 +98,30 @@ INSERT INTO `newsarticles` (`idNews`, `name`, `mail`, `subject`, `descriptionArt
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `trn_date` datetime NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `trn_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_type` varchar(100) DEFAULT 'membre',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `trn_date`) VALUES
-(1, 'ibra-yves', 'ibrahima.conde@hotmail.com', '098f6bcd4621d373cade4e832627b4f6', '2020-03-08 20:15:53'),
-(4, 'maxime', 'coronavirus@hotmail.com', 'test', '2020-03-10 11:49:18'),
-(3, 'Toto', 'ibrahima.conde@hotmail.com', '098f6bcd4621d373cade4e832627b4f6', '2020-03-08 20:31:32'),
-(5, 'test1', 'test1@hotmail.com', '098f6bcd4621d373cade4e832627b4f6', '2020-03-11 09:36:27'),
-(6, 'test', 'test@hotmail.com', '098f6bcd4621d373cade4e832627b4f6', '2020-03-19 13:03:53'),
-(7, 'maxime', 'm@outlook.com', 'b238c13e822536cad3ac57a2280fbf45', '2020-03-19 13:30:58'),
-(8, 'maxibra', 'mxm.lbr@outlook.com', '098f6bcd4621d373cade4e832627b4f6', '2020-04-05 12:10:47'),
-(9, 'pute', 'pute@hotmail.com', '098f6bcd4621d373cade4e832627b4f6', '2020-04-05 12:46:01'),
-(10, 'liberm', 'test@hotmail.com', 'accc9105df5383111407fd5b41255e23', '2020-04-11 12:02:51'),
-(11, 'IBRACRIE', 'ibracrie@hotmail.com', '098f6bcd4621d373cade4e832627b4f6', '2020-04-16 16:08:38'),
-(12, 'maxtest', 'maxtest@pute.com', '098f6bcd4621d373cade4e832627b4f6', '2020-04-16 17:28:12'),
-(13, 'soso', 'soso@hotmail.com', '098f6bcd4621d373cade4e832627b4f6', '2020-04-16 17:55:12'),
-(14, 'ceciestuntest', 'mxm.lbr@test.com', 'f6fb5eeb3e0bd744eaf3e8d76b5783ab', '2020-04-19 11:04:01');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `trn_date`, `user_type`) VALUES
+(1, 'alexis', 'alexis@hotmail.com', '059bf68f71c80fce55214b411dd2280c', '2020-05-19 13:49:08', 'admin');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `entrainement`
+--
+ALTER TABLE `entrainement`
+  ADD CONSTRAINT `idUsers` FOREIGN KEY (`idUsers`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
