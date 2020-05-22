a98953a6fd021caada247e101cbf5f96abff63ee
Ibrahima <44869843+Ibra-Yves@users.noreply.github.com>
2020-05-22 18:45:35 +0200

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,15 @@
+<title>Caponweb - Fatal error</title>
+        <section class="banner_area">
+            <div class="box_1620">
+				<div class="banner_inner d-flex align-items-center">
+					<div class="container">
+						<div class="banner_content text-center">
+                            <h2>Error 404</h2>
+							<h2>Interdiction d'accés aux fichiers de ce répertoire</h2>
+						</div>
+					</div>
+				</div>
+            </div>
+        </section>
+    </body>
+</html>
